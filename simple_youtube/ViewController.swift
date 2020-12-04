8d6553de07d2c042a5c2a1e320fc740ca01b4483
pazrvien <yjaewoong@naver.com>
2020-12-04 13:29:32 +0900

    initial xcode project creation

---
+++ simple_youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_youtube
+//
+//  Created by 윤재웅 on 2020/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
