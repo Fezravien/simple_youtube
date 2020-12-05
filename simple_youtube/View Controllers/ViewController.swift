2f2047a9658c74aa8d0f6ae51ae0080ab0c0b3b1
pazrvien <yjaewoong@naver.com>
2020-12-05 16:40:03 +0900

    Added detailviewController, segue to detail, and webview in detailviewController

--- simple_youtube/View Controllers/ViewController.swift
+++ simple_youtube/View Controllers/ViewController.swift
@@ -31,6 +31,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Confirm that a video was selected
+        guard tableVIew.indexPathForSelectedRow != nil else {
+            return
+        }
+        
+        // Get a reference to the video that was tapped on
+        let selectedVideo = videos[tableVIew.indexPathForSelectedRow!.row]
+        
+        // Get a reference to detail view controller
+        let detailVC = segue.destination as! DetailViewController
+        
+        // Set the video property of the detail view controller
+        detailVC.video = selectedVideo
+        
+    }
+    
     // MARK: - Model Delegate Methods
     func videosFetched(_ videos: [Video]) {
         // Set the returned videos to our video property
